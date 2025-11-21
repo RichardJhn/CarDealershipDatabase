f21522fc985604d9262da3fb596ab33e036b36cd
RichardJhn <rjohn@my.yearupunited.org>
2025-11-21 15:29:24 -0500

    made database and 2 tables

---
+++ tableDealership.sql
@@ -0,0 +1,8 @@
+use cardealership;
+
+CREATE TABLE dealership(
+dealership_id int auto_increment primary key,
+name varchar (50),
+address varchar(50),
+phone varchar(12)
+  );
---
+++ tableVehicles.sql
@@ -0,0 +1,8 @@
+use cardealership;
+
+CREATE TABLE vehicles (
+carmakemodel varchar(50),
+carcolor varchar(50),
+mileage int,
+vin int primary key
+);
